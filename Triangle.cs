7c72ee1b781e6e112a80caeae3dae50379773e23
rinera182@gmail.com <rinera182@gmail.com>
2021-11-23 23:28:23 +0900

    Triangle

---
+++ Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorSampe
+{
+    class Triangle
+    {
+        private float bottom;
+        private float height;
+        public Triangle(float b, float h)
+        {
+            this.bottom = b;
+            this.height = h;
+        }
+        public float GetSurface()
+        {
+            return bottom * height / 2;
+        }
+    }
+}
